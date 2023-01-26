fc80895008ae0350904e3c79f6077887cc172a95
Faris Abufarha <70337488+faris771@users.noreply.github.com>
2023-01-26 19:02:04 +0200

    Change #3
    changed some realtions (check new erd)

--- HotelDataBase.sql
+++ HotelDataBase.sql
@@ -4,30 +4,30 @@ USE HOTEL_COMP333;
 
 create table employee (
 
-  eid int primary key AUTO_INCREMENT,
-  employee_SSN int UNIQUE,
+  eid int primary key AUTO_INCREMENT, -- 1 2 3 
+  employee_SSN int UNIQUE,  
   employee_first_Name varchar(32),
   employee_father_Name varchar(32),
-  employee_grandfather_Name varchar(32),
   employee_family_Name varchar(32),
   employee_email varchar(32),
-  employee_nationality varchar(32)
+  employee_nationality varchar(32),
+  starting_date DATE DEFAULT(CURRENT_DATE()) ,
   salary real
 
 );
+INSERT INTO EMPLOYEE VALUES(1,1,"root",'root','root','root@gmail.com','PS',CURRENT_DATE(), 1000.0);
 
 create table services(
   service_id int primary key AUTO_INCREMENT,
   service_type varchar(32),
   service_cost real DEFAULT 0.00,
-  item_name varchar(32) -- ???
+  serving_date_time DATETIME DEFAULT NOW()
 );
 
 create table Guest(
   Guest_SSN int primary key,
   Guest_first_Name varchar(32),
   Guest_father_Name varchar(32),
-  Guest_grandfather_Name varchar(32),
   Guest_family_Name varchar(32),
 
   Guest_email varchar(32),
@@ -41,7 +41,7 @@ CREATE TABLE  employee_phone(
 	phone_num VARCHAR(32) ,
   eid INT,
   PRIMARY KEY(phone_num,eid),
-  FOREIGN KEY(eid) REFERENCES EMPLOYEE(eid) ON DELETE CASCADE
+  FOREIGN KEY(eid) REFERENCES EMPLOYEE(eid)
 );
 
 
@@ -49,18 +49,18 @@ create table room (
   room_number int primary key,
   room_price real,
   number_of_beds INT,
-  beds_type VARCHAR(32)
+  room_type VARCHAR(32),
   room_status VARCHAR(32) -- OCCUPIED OR NOT
 );
 
 
 create table service_to_room( -- SAME AS 'PROVIDE'
 	-- suggestion : provides service to a room not a guest
-  room_id int,
+  room_number int,
   eid int,
   service_id int,
-  primary key (room_id, eid, serviceid),
-  foreign key (room_id) references room (room_id),
+  primary key (room_number, eid, service_id),
+  foreign key (room_number) references room (room_number),
   foreign key (eid) references employee (eid),
   foreign key (service_id) references services(service_id)
 
@@ -68,34 +68,26 @@ create table service_to_room( -- SAME AS 'PROVIDE'
 
 
 
-
 create table Booking(
   Booking_id int primary key AUTO_INCREMENT,
   room_number INT,
   guest_ssn INT,
-  start_date date,
-  end_date date
-  FOREIGN KEY(room_number) REFERENCES room (room_number)
-  FOREIGN KEY(guest_ssn) REFERENCES room (guest_ssn)
+  STARTING_DATE date,
+  end_date date,
+  FOREIGN KEY(room_number) REFERENCES room (room_number),
+  FOREIGN KEY(guest_ssn) REFERENCES guest (guest_ssn)
 );
 
 
 CREATE TABLE PAYMENT(
+
 	Payment_id INT PRIMARY KEY AUTO_INCREMENT,
+  guest_ssn int,
+  Booking_id int,
   Payment_way varchar(32),
 	Payment_Day varchar (32),
+  foreign key (Booking_id) references Booking (Booking_id),
+  foreign key (guest_ssn) references Booking (guest_ssn)
 
 );
-
-
-create table Pay_Payment (
-  payment_id int primary key AUTO_INCREMENT,
-  Payment_way varchar(32),
-  Payment_Day varchar (32),
-  Booking_id int,
-  foreign key (Booking_id) references Booking (Booking_id)
-);
-
-
-
 -- 
