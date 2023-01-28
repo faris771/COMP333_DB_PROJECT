4cdae6f2843c27dcaa2348371c6b01eade053672
hawashra <hamzaawashra100@gmail.com>
2023-01-28 12:48:55 +0200

    First commit Hamza

--- src/main/java/com/example/comp333/HelloApplication.java
+++ src/main/java/com/example/comp333/HelloApplication.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+// Hamza Awashra
 import java.io.IOException;
 
 public class HelloApplication extends Application {
