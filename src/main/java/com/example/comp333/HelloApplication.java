aef6c0636b27a462ebf04f8befce2ec9d22845dc
Shereen <@shereenibdah>
2023-01-29 19:37:04 +0200

    add comment

--- src/main/java/com/example/comp333/HelloApplication.java
+++ src/main/java/com/example/comp333/HelloApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 // testing pull
 public class HelloApplication extends Application {
+    //test
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
