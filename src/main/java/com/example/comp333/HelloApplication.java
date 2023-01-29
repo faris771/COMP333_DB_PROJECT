64a90bad2847f38101dacc08fbb8eb9657180533
Shereen <@shereenibdah>
2023-01-29 20:09:07 +0200

    first scene (log in )

--- src/main/java/com/example/comp333/HelloApplication.java
+++ src/main/java/com/example/comp333/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 410,500);
         stage.setTitle("Hotel DataBase!");
         stage.setScene(scene);
         stage.show();
