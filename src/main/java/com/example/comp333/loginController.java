0aa79aeeb9f2d40e00c9ab42dfd873a0ee470688
hawashra <hamzaawashra100@gmail.com>
2023-01-30 18:28:59 +0200

    added switch between login and menu

--- src/main/java/com/example/comp333/loginController.java
+++ src/main/java/com/example/comp333/loginController.java
@@ -2,6 +2,8 @@ package com.example.comp333;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,6 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class loginController {
@@ -28,6 +31,10 @@ public class loginController {
     @FXML
     Label tryAgainLabel;
 
+    @FXML
+    private  ActionEvent event;
+
+
     public void clearButtonOnAction(ActionEvent event) {
         userName.setText("");
         password.setText("");
@@ -36,14 +43,14 @@ public class loginController {
 
     public void loginButtonOnAction(ActionEvent event) throws SQLException { //throws IOException
         if (!userName.getText().isBlank() && !password.getText().isBlank()) { // if username and password are not blank
-            validateLogin(); // call validateLogin method
+            validateLogin(event); // call validateLogin method
         } else {
             tryAgainLabel.setText("PLEASE INPUT USERNAME AND PASSWORD"); // if username and password are blank
             tryAgainLabel.setTextFill(Color.RED);
         }
     }
 
-    public void validateLogin() throws SQLException {
+    public void validateLogin(ActionEvent event) throws SQLException {
 
         DataBaseConnection connectNow = new DataBaseConnection(); // create new object of DataBaseConnection class
         Connection connectDB = connectNow.getConnection();
@@ -59,9 +66,22 @@ public class loginController {
                 tryAgainLabel.setText("WELCOME");
                 tryAgainLabel.setTextFill(Color.GREEN);
                 /*
-                    SWITCH SCENE HERE
+                    SWITCH SCENE HERE */
+                    try {
 
-                */
+                        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuScene.fxml"));
+                        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                        // for menu Scene put the size 500 , 600
+                        scene = new Scene(fxmlLoader.load(), 500, 600);
+                        stage.setTitle("Hotel DataBase!");
+                        stage.setScene(scene);
+                        stage.show();
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+
             } else {
                 tryAgainLabel.setText("WRONG USERNAME OR PASSWORD TRY AGAIN");
                 tryAgainLabel.setTextFill(Color.RED);
